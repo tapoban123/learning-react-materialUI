1f2bef99e2adef4dc3855ce345828a4e76224253
Tapoban Ray <raytapoban@gmail.com>
2025-02-05 17:07:21 +0530

    Built one page

--- src/App.js
+++ src/App.js
@@ -2,17 +2,33 @@ import "./App.css";
 import TourCard from "./components/TourCard";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid2";
+import SearchAppBar from "./components/AppBar";
+import cities from "./data.json";
+import { Typography } from "@mui/material";
 
 function App() {
   return (
     <div className="App">
-      <Container>
-        <Grid container spacing={5}>
-          <TourCard/>
-          <TourCard/>
-          <TourCard/>
-          <TourCard/>
-        </Grid>
+      <SearchAppBar />
+      <Container sx={{ marginY: 5 }}>
+        {cities.map((city) => (
+          <>
+            <Typography
+              variant="h4"
+              component={"h2"}
+              marginTop={5}
+              marginBottom={3}
+            >
+              Top {city.name} Tours
+            </Typography>
+
+            <Grid container spacing={5}>
+              {city.tours.map((tour, index) => (
+                <TourCard tour={tour} key={index} />
+              ))}
+            </Grid>
+          </>
+        ))}
       </Container>
     </div>
   );
--- src/components/TourCard.js
+++ src/components/TourCard.js
@@ -2,23 +2,85 @@ import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid2";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
+import { AccessTime } from "@mui/icons-material";
+import Rating from "@mui/material/Rating";
+import { createTheme, ThemeProvider } from "@mui/material";
 
-const TourCard = () => {
+const theme = createTheme({
+  components: {
+    MuiTypography: {
+      variants: [
+        {
+          props: {
+            variant: "body2",
+          },
+          style: {
+            fontSize: 11,
+          },
+        },
+        {
+          props: {
+            variant: "body3",
+          },
+          style: {
+            fontSize: 9,
+          },
+        },
+      ],
+    },
+  },
+});
+
+const TourCard = (tour) => {
   return (
     <Grid size={3}>
-      <Paper elevation={3}>
-        <img
-          src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0d/a3/8b/5f/niagara-falls.jpg?w=900&h=-1&s=1"
-          alt=""
-          className="img"
-        ></img>
-        <Box paddingX={1}>
-          <Typography variant="subtitle1" component={"h2"}>
-            Immerse into the falls
-          </Typography>
-          <Typography variant="body2" component={"p"}>5 hours</Typography>
-        </Box>
-      </Paper>
+      <ThemeProvider theme={theme}>
+        <Paper elevation={3}>
+          <img src={tour.image} alt="" className="img"></img>
+          <Box paddingX={1}>
+            <Typography variant="subtitle1" component={"h2"}>
+              {tour.name}
+            </Typography>
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+              }}
+            >
+              <AccessTime width={12.5}></AccessTime>
+              <Typography variant="body2" component={"p"}>
+                {tour.duration} hours
+              </Typography>
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                alignitems: "center",
+              }}
+              marginTop={3}
+              alignItems={"center"}
+            >
+              <Rating
+                name="half-rating"
+                value={tour.rating}
+                readOnly
+                precision={0.5}
+              />
+              <Typography variant="body2" component={"p"} marginLeft={0.5}>
+                {tour.rating}
+              </Typography>
+              <Typography variant="body3" component={"p"} marginLeft={1.5}>
+                ({tour.numberOfReviews} reviews)
+              </Typography>
+            </Box>
+            <Box>
+              <Typography variant="h6" component={"h3"} marginTop={0}>
+                From C ${tour.price}
+              </Typography>
+            </Box>
+          </Box>
+        </Paper>
+      </ThemeProvider>
     </Grid>
   );
 };

b55c4f6677a002fdcfa2828961a2658a9f6d1924
Tapoban Ray <raytapoban@gmail.com>
2025-02-07 01:11:28 +0530

    Created SecondPage

--- src/pages/Tour.js
+++ src/pages/Tour.js
@@ -1,3 +1,60 @@
-const Tour = () => <div>Hello World</div>;
+import { Container, Typography } from "@mui/material";
+import Box from "@mui/material/Box";
+import ImageCollage from "../components/ImageCollage";
+import CustomizedAccordion from "../components/Accordian";
+import Paper from "@mui/material/Paper";
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BasicModal from "../components/Modal";
+
+const Tour = () => (
+  <Container sx={{ width: 900 }}>
+    <Typography variant="h3" component={"h1"} marginTop={3}>
+      Explore the World in Vegas
+    </Typography>
+    <Box marginTop={3} sx={{ display: "flex" }}>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/f/f7/Las_Vegas_Sign_Nightly.jpg"
+        alt=""
+        height={325}
+        width={570}
+      ></img>
+      <ImageCollage />
+    </Box>
+    <Box>
+      <Typography variant="h6" component={"h4"} marginTop={3}>
+        About this ticket
+      </Typography>
+      <Typography variant="paragraph" component={"p"} marginTop={3}>
+        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+        accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
+        illo inventore veritatis et quasi architecto beatae vitae dicta sunt
+        explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut
+        odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
+        voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum
+        quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam
+        eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat
+        voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam
+        corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
+        Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse
+        quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo
+        voluptas nulla pariatur
+      </Typography>
+    </Box>
+    <Box marginBottom={10}>
+      <Typography variant="h6" component={"h4"} marginTop={3} marginBottom={2}>
+        Frequently Asked Questions
+      </Typography>
+      <CustomizedAccordion></CustomizedAccordion>
+    </Box>
+    <Paper
+      sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
+      elevation={3}
+    >
+      <BottomNavigation>
+        <BasicModal />
+      </BottomNavigation>
+    </Paper>
+  </Container>
+);
 
 export default Tour;
